3ce4f86398b0297a99fde624df5cbaf94c329a5e
David Hakobyan <davidhakobyan222@gmail.com>
2025-06-16 17:24:34 +0400

    initial commit

---
+++ hello.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    my_name = "Ben"
+    stuff = "This is Bold text"
+
+    favorite_pizza = ["Pepperoni", "cheese", "altono", 41]
+    return render_template("index.html", my_name=my_name, 
+                           stuff=stuff, 
+                           favorite_pizza=favorite_pizza)
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", user_name=name)
+
+
+
+#Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
